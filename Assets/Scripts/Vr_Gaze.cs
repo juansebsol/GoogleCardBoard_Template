ad2a56bcb209a279c946675d7c6dabbe35c0f021
juansebsol <juan_sebastiansolano@Hotmail.com>
2020-05-13 17:54:16 -0400

    .

--- Assets/Scripts/Vr_Gaze.cs
+++ Assets/Scripts/Vr_Gaze.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,6 +12,9 @@ public class Vr_Gaze : MonoBehaviour
     public float totalTime = 2;
     bool gvrStatus;
     float gvrTimer;
+
+    public int distanceOfRay = 10;
+    private RaycastHit _hit;
 
     //Use this for initialization
     void Start()
@@ -25,6 +29,15 @@ public class Vr_Gaze : MonoBehaviour
         {
             gvrTimer += Time.deltaTime;
             imgGaze.fillAmount = gvrTimer / totalTime;
+        }
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        if (Physics.Raycast(ray, out _hit, distanceOfRay)) 
+        {
+            if (imgGaze.fillAmount == 1 && _hit.transform.CompareTag("Teleport")) 
+            {
+                _hit.transform.gameObject.GetComponent<Teleport>().TeleportPlayer();
+            
+            }
         }
     }
 
